798248574210a9fbdc186aa7bda8518c6d8f77a1
serii <seriiburduja@gmail.com>
2018-03-08 20:21:17 +0200

    first commit

---
+++ 01/classwork/App/Db.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: serii
+ * Date: 3/8/18
+ * Time: 8:13 PM
+ */
+
+namespace App;
+
+
+class Db
+{
+    public function __construct()
+    {
+        echo "hello world";
+    }
+}
